3ea8bff4106ecc6227cb0e83daddf385b494be97
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-12-04 22:12:27 +0700

    DA_Cap nhat du lieu phan quyen

--- QuanLyKhachSan/GUI/Admin_GUI.cs
+++ QuanLyKhachSan/GUI/Admin_GUI.cs
@@ -45,6 +45,72 @@ namespace QuanLyKhachSan.GUI
             cbmanv.ValueMember = "manv";
             cbmanv.DisplayMember = "manv";
         }
+        private void Admin_GUI_Load(object sender, EventArgs e)
+        {
+            bindDataAd();
+            bindDataCbNV();
+            if (db.checkExist("quyennv", "manv", cbmanv.SelectedValue.ToString()))
+            {
+                txtqh.Text = adbl.quyennv(cbmanv.SelectedValue.ToString());
+            }
+            else
+            {
+                txtqh.Text = "Chưa phân quyền";
+            }
+        }
 
+        private void cbmanv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (db.checkExist("quyennv", "manv", cbmanv.SelectedValue.ToString()))
+            {
+                txtqh.Text = adbl.quyennv(cbmanv.SelectedValue.ToString());
+            }
+            else
+            {
+                txtqh.Text = "Chưa phân quyền";
+            }
+        }
+
+        private void btnPhanQuyen_Click(object sender, EventArgs e)
+        {
+            panelpq.Enabled = true;
+        }
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            string sql = "Insert Into quyennv Values('" + cbmanv.SelectedValue.ToString() + "','" + id + "','" + txtmknv.Text + "')";
+            string sql2 = "Update quyennv set id='" + id + "',matkhau='" + txtmknv.Text + "' where manv = '" + cbmanv.SelectedValue.ToString() + "'";
+            if (txtmknv.Text.Length == 3)
+            {
+                thongbao.Text = "";
+                if (!db.checkExist("quyennv", "manv", cbmanv.SelectedValue.ToString()))
+                {
+                    if (db.ExecuteQuery(sql))
+                    {
+                        rdbqt.Checked = false;
+                        rdbnv.Checked = false;
+                        rdbkhach.Checked = false;
+                        txtmknv.Text = "";
+                        panelpq.Enabled = false;
+                        thongbao.Text = "";
+                    }
+                }
+                else
+                {
+                    if (db.ExecuteQuery(sql2))
+                    {
+                        rdbqt.Checked = false;
+                        rdbnv.Checked = false;
+                        rdbkhach.Checked = false;
+                        txtmknv.Text = "";
+                        panelpq.Enabled = false;
+                        thongbao.Text = "";
+                    }
+                }
+            }
+            else
+            {
+                thongbao.Text = "Mật khẩu gồm 3 ký tự!";
+            }
+        }
     }
 }
