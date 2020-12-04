2a476f641eb0e0067632155aa5e6aad4f827a847
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-12-04 21:25:54 +0700

    TL_ Bat su kien cho cac button trong form trang chu

--- QuanLyKhachSan/DAL/DBAccess.cs
+++ QuanLyKhachSan/DAL/DBAccess.cs
@@ -23,7 +23,7 @@ namespace QuanLyKhachSan.DAL
         //Kết nối
         public void connectDB()
         {
-            string strCon = @"Data Source=DESKTOP-UR6D0PD\SQLEXPRESS;Initial Catalog=QLKSC;Integrated Security=True";
+            string strCon = @"Data Source=.\SQLEXPRESS;Initial Catalog=QLKSC;Integrated Security=True";
             con = new SqlConnection(strCon);
             try
             {
