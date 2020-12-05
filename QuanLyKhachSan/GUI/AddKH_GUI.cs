94755c5df756ba8772a71590e0494d7222a54f88
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-05 22:59:21 +0700

    BT_Them du lieu

--- QuanLyKhachSan/GUI/AddKH_GUI.cs
+++ QuanLyKhachSan/GUI/AddKH_GUI.cs
@@ -16,13 +16,36 @@ namespace QuanLyKhachSan.GUI
 {
     public partial class AddKH_GUI : DevExpress.XtraEditors.XtraUserControl
     {
-
+        KhachHang_BLL khbl = new KhachHang_BLL();
+        DBAccess db = new DBAccess();
+        int t = 0;
 
         public AddKH_GUI()
         {
             InitializeComponent();
         }
 
-     
+        private KhachHang_DTO getdatakh()
+        {
+            KhachHang_DTO kh = new KhachHang_DTO();
+            kh.Makh = txtmakh.Text;
+            kh.Hoten = txthoten.Text;
+            kh.Cmnd = txtcmnd.Text;
+            kh.Sdt = txtsdt.Text;
+            kh.Email = txtemail.Text;
+            kh.Diachi = txtdc.Text;
+            return kh;
+        }
+
+        private void showtxtkh()
+        {
+            txthoten.Enabled = true;
+            txtcmnd.Enabled = true;
+            txtsdt.Enabled = true;
+            txtemail.Enabled = true;
+            txtdc.Enabled = true;
+        }
+
+       
     }
 }
