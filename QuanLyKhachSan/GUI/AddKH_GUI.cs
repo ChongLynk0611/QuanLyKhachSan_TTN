7c1f74261191458d45d305e3136d64e9dec493f0
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-05 14:30:43 +0700

    BT_Tao form them khach hang

---
+++ QuanLyKhachSan/GUI/AddKH_GUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyKhachSan.BLL;
+using QuanLyKhachSan.DTO;
+using QuanLyKhachSan.DAL;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class AddKH_GUI : DevExpress.XtraEditors.XtraUserControl
+    {
+
+
+        public AddKH_GUI()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
