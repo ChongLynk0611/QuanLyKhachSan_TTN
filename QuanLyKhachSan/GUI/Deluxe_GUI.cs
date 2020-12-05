53d513bdfcb59a5ef7f917552f2ca2d943fe5059
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-12-05 23:03:29 +0700

    TQ_De_Tao giao dien Phong Deluxe

---
+++ QuanLyKhachSan/GUI/Deluxe_GUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyKhachSan.BLL;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class Deluxe_GUI : DevExpress.XtraEditors.XtraUserControl
+    {
+        Phong_BLL pbl = new Phong_BLL();
+
+        public Deluxe_GUI()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
