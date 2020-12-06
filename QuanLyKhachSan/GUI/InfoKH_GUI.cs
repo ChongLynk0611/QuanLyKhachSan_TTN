ee357aa088176156b4efe5301a25f17e49881047
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-12-06 08:12:49 +0700

    DA_Khoi tao Form InfoKH

---
+++ QuanLyKhachSan/GUI/InfoKH_GUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyKhachSan.DAL;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class InfoKH_GUI : DevExpress.XtraEditors.XtraUserControl
+    { 
+        public InfoKH_GUI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
