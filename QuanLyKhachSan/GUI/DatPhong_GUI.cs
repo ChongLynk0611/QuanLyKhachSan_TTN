53b62f2cb129be650ce93f24e92cb9b54a1e6e4c
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-06 17:09:12 +0700

    BT_Tao form dat phong

---
+++ QuanLyKhachSan/GUI/DatPhong_GUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyKhachSan.BLL;
+using QuanLyKhachSan.DTO;
+using QuanLyKhachSan.DAL;
+using System.Collections;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class DatPhong_GUI : DevExpress.XtraEditors.XtraUserControl
+    {
+
+        
+        public DatPhong_GUI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
