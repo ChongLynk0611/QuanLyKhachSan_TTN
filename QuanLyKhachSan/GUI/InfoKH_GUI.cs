3b6ed9cd79e3852457ed2f177dcb551418c4e8bb
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-12-06 16:13:57 +0700

    DA_Thiet lap KH coban & nangcao

--- QuanLyKhachSan/GUI/InfoKH_GUI.cs
+++ QuanLyKhachSan/GUI/InfoKH_GUI.cs
@@ -13,11 +13,31 @@ using QuanLyKhachSan.DAL;
 namespace QuanLyKhachSan.GUI
 {
     public partial class InfoKH_GUI : DevExpress.XtraEditors.XtraUserControl
-    { 
+    {
+        DBAccess db = new DBAccess();
+        string and_or1 = "";
+        string and_or2 = "";
+        string dk0 = "";
+        string dk1 = "";
+        string dk2 = "";
+        string dk3 = "";
+        string txt0 = "";
+        string txt1 = "";
+        string txt2 = "";
+        string txt3 = "";
         public InfoKH_GUI()
         {
             InitializeComponent();
         }
-
+        private void gettxtcb()
+        {
+            txt0 = txtCoBan.Text;
+        }
+        private void gettxtnc()
+        {
+            txt1 = txtNangCao1.Text;
+            txt2 = txtNangCao2.Text;
+            txt3 = txtNangCao3.Text;
+        }
     }
 }
