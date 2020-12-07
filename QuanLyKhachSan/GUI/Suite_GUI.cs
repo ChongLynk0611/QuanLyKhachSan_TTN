4f66d4d31cf37af14f89cbb70ae0a8674310e6c1
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-12-07 08:22:01 +0700

    TQ_Su_Tao giao dien phong Suite

---
+++ QuanLyKhachSan/GUI/Suite_GUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanLyKhachSan.BLL;
+
+namespace QuanLyKhachSan.GUI
+{
+    public partial class Suite_GUI : DevExpress.XtraEditors.XtraUserControl
+    {
+        Phong_BLL pbl = new Phong_BLL();
+        public Suite_GUI()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
